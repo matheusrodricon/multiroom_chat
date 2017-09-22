70eee358f28a979d6b8ffc45941bd5809b11c413
Matheus RC <matheusrodri@gmail.com>
2017-09-21 22:36:00 -0300

    Validação do form de entrada e inicio implementação de websockets

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 var app = require('./config/server');
 
 // parametrizar a porta de escuta
-app.listen(80, function() {
+var server = app.listen(80, function() {
 	console.log("Servidor online");
+});
+
+var io = require('socket.io').listen(server);
+
+/* Criar a conexão por WebSocket */
+io.on('connection', function(socket) {
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function() {
+		console.log('Usuário desconectou')
+	})
 });
