14077a0e389e21aa5946b1d1b7aeb56b3dda4f3f
Matheus RC <matheusrodri@gmail.com>
2017-09-22 18:29:43 -0300

    Multiroom chat finalizado

--- app.js
+++ app.js
@@ -8,11 +8,43 @@ var server = app.listen(80, function() {
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 /* Criar a conexão por WebSocket */
 io.on('connection', function(socket) {
 	console.log('Usuário conectou');
 
+	
 	socket.on('disconnect', function() {
 		console.log('Usuário desconectou')
-	})
+	});
+
+	socket.on('msgParaServidor', function(data) {
+		
+		/* Diálogo */
+		socket.emit(
+			'msgParaCliente',
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		socket.broadcast.emit(
+			'msgParaCliente',
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+
+		/* Participantes */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+			socket.emit(
+				'participantesParaCliente',
+				{apelido: data.apelido}
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente',
+				{apelido: data.apelido}
+			);
+		}	
+
+	});
 });
